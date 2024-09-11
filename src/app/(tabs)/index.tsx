f10eb42347964642c5ced13e4742c43baf84e7a4
Prayag Joshi <prayagjoshi263@gmail.com>
2024-09-11 14:28:14 +0530

    feat: "reanmed components"

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Link, Redirect } from "expo-router";
 
 export default function Home() {
   return <Text>Feed</Text>;
--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-export default function New() {
+export default function CreatePost() {
   return <Text>Add new post</Text>;
 }
 
