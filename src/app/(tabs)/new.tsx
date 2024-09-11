62042b15448eef36e3489b0dc21681748a2db601
Prayag Joshi <prayagjoshi263@gmail.com>
2024-09-11 14:25:10 +0530

    Install Expo Router and setup Tab navigation structure

---
+++ src/app/(tabs)/new.tsx
@@ -0,0 +1,8 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function New() {
+  return <Text>Add new post</Text>;
+}
+
+const styles = StyleSheet.create({});
---
+++ src/app/(tabs)/profile.tsx
@@ -0,0 +1,8 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Profile() {
+  return <Text>Profile</Text>;
+}
+
+const styles = StyleSheet.create({});
