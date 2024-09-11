4c15a6a48742f1037b9481ca96284083d137c34f
Prayag Joshi <prayagjoshi263@gmail.com>
2024-09-11 14:51:06 +0530

    native wind setup

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,7 +2,11 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 export default function Home() {
-  return <Text>Feed</Text>;
+  return (
+    <View className="flex-1 bg-gray-700">
+      <Text className="text-4xl color-red-600">Feed</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
